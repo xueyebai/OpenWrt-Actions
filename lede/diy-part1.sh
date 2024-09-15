fe9e3da839f77f311cc0fa52ba1b7dfd3d06796d
xueyebai <zhongrenwei@gmail.com>
2024-09-16 03:15:15 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -18,14 +18,13 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> 'feeds.co
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> 'feeds.conf.default'
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> 'feeds.conf.default'
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> 'feeds.conf.default'
+echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' >>feeds.conf.default
 
 # add plugins
-cd package/lean
-rm -rf *argon* *mosdns*
+mkdir package/js
+cd package/js
 git clone -b 18.06 https://github.com/xueyebai/luci-theme-argon.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
-wget https://github.com/sbwml/luci-app-mosdns/archive/refs/tags/v5.3.1.tar.gz
-tar -zxvf *tar.gz
-git clone -b master https://github.com/fw876/helloworld
-git clone https://github.com/sbwml/v2ray-geodata
+git clone  -b v5 https://github.com/sbwml/luci-app-mosdns.git
+git clone https://github.com/sbwml/v2ray-geodata.git
