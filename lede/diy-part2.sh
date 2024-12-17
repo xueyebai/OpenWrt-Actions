c1a40ddc3a734df49f4d9b7ef78bd030b593de85
xueyebai <zhongrenwei@gmail.com>
2024-12-17 16:25:26 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -10,6 +10,7 @@ rm -rf *passwall*
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git
+# 移动目录到上级目录
 mv openwrt-passwall/luci-app-passwall .
 # mv openwrt-passwall2/luci-app-passwall2 .
 rm -rf *openwrt-pa*
